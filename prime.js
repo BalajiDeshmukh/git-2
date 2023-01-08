06264e0c25eac2bc0266e4e907dad1aa4b207746
Kiran wasarkar <you@example.com>
2023-01-08 19:27:32 +0530

    prime number

---
+++ prime.js
@@ -0,0 +1,14 @@
+let number=3;
+let factor=0;
+for(let i=0; i<N; i++){
+if(3%i==0){
+factor++;
+}
+}
+if(factor==0){
+console.log("number is prime");
+}
+else{
+console.log("number is not a prime");
+}
+
